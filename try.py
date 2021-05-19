4e3e3d52711232b3c47d980f9e4f88414215911d
Dekel-Katz <84238555+Dekel-Katz@users.noreply.github.com>
2021-05-19 16:35:42 +0300

    Update try.py

--- try.py
+++ try.py
@@ -1,50 +1,44 @@
-from selenium.common.exceptions import NoSuchElementException, \
-    WebDriverException
+from selenium.common.exceptions import ElementClickInterceptedException, \
+    NoSuchElementException, WebDriverException
 from selenium import webdriver as wb
 import pandas as pd
 
-decades = ['1980', '1990', '2000', '2010', '2020']
 
 webD = wb.Chrome('chromedriver.exe')
 
 
 def get_dates():
     songs = pd.read_json('songs_dt.json', orient='table')
-    dates = list(songs["Date"])
+    dates = list(songs['Date'])
     albums = songs["Album"]
+    artists = songs["Artist"]
+    found_albums = {}
     start = 0
     try:
-        for decade in decades:
-            for num in range(0, 10):
-                album_names = []
-                year = decade[:3]+str(num)
-                print(year)
-                if year == '2022':
-                    break
-                url = 'https://www.discogs.com/search/?limit=250&genre_exact' \
-                      '=Hip+Hop&decade=' + decade + '&year=' + year + \
-                      '&layout=sm&type=release'
-                while True:
-                    webD.get(url)
-                    if start == 0:
-                        webD.find_element_by_xpath(
-                            '/html/body/div[6]/div[3]/div/div['
-                            '1]/div/div[2]/div/button[2]').click()
-                        webD.find_element_by_xpath(
-                            '/html/body/div[6]/div[2]/div[3]/div[1]/button').click()
-                        start = 1
-                    for name in webD.find_elements_by_class_name(
-                            'search_result_title'):
-                        album_names.append(name.text.split(' (')[0].upper())
-                    try:
-                        url = webD.find_element_by_class_name(
-                            'pagination_next').get_attribute('href')
-                    except NoSuchElementException:
-                        break
-                for index in range(len(albums)):
-                    if albums[index].split(' (')[0].upper() in album_names:
-                        if type(dates[index]) is not float and type(dates[index]) is not float:
-                            dates[index] = int(year)
+        for index in range(len(albums)):
+            if type(dates[index]) is not int and type(dates[index]) is not \
+                    float:
+                continue
+            elif albums[index].upper() not in found_albums:
+                webD.get('https://www.discogs.com/search/?q=' + albums[index].split(
+                    '(')[0].replace(' ', '+') + '+' + artists[index].replace(
+                    ' ', '+') + '&type=release&layout=sm')
+                if start == 0:
+                    webD.find_element_by_xpath('/html/body/div[6]/div[3]/div/div['
+                                               '1]/div/div[2]/div/button[2]').click()
+                    webD.find_element_by_xpath(
+                        '/html/body/div[6]/div[2]/div[3]/div[1]/button').click()
+                    start = 1
+                try:
+                    year = int(webD.find_element_by_class_name('card_release_year').text[-4:])
+                    if year < 1980:
+                        year = None
+                except ElementClickInterceptedException:
+                    year = None
+                except NoSuchElementException:
+                    year = None
+                found_albums[albums[index].upper()] = year
+                dates[index] = year
         songs = songs.assign(Date=dates)
         songs.to_json('songs_dt2.json', orient='table', indent=4)
     except WebDriverException:
@@ -52,5 +46,5 @@ def get_dates():
         songs.to_json('songs_dt2.json', orient='table', indent=4)
 
 
-
-get_dates()
+if __name__ == "__main__":
+    get_dates()
