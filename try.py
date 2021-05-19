7142a6811695fb3a216bb1f2bd2ce4f080439e23
Dekel-Katz <84238555+Dekel-Katz@users.noreply.github.com>
2021-05-19 13:02:22 +0300

    Update try.py
    Working code for updating the years. If there is a disconnect, it will update the file from what it already saw. You can activate it again then, with another line for skipping the years it did already

--- try.py
+++ try.py
@@ -1,41 +1,56 @@
-from bs4 import BeautifulSoup
-import requests
+from selenium.common.exceptions import NoSuchElementException, \
+    WebDriverException
+from selenium import webdriver as wb
 import pandas as pd
 
+decades = ['1980', '1990', '2000', '2010', '2020']
 
-def get_dates(url):
+webD = wb.Chrome('chromedriver.exe')
+
+
+def get_dates():
     songs = pd.read_json('songs_dt.json', orient='table')
-    dates = []
-    album_dates = {}
+    dates = list(songs["Date"])
     albums = songs["Album"]
-    artists = songs["Artist"]
-    rap_albums = []
-    for index in range(1, 35):
-        html = requests.get(url).text
-        soup = BeautifulSoup(html, 'html.parser')
-        rap_albums = rap_albums + soup.find_all('div', class_="itemview")[:499]
-        url = "https://www.rapmusicguide.com/browse/p/6/sb/ar|ASC|500|" + \
-              str(index)
-    for index in range(len(albums)):
-        if albums[index].upper() not in album_dates:
-            for rap_album in rap_albums:
-                album_dates[albums[index].upper()] = None
-                if albums[index].split('(')[0].upper() in \
-                        rap_album.find_all('a')[1].text.upper() and artists[
-                        index] in rap_album.find_all('a')[0].text.upper():
-                    album_site = rap_album.find_all('a')[1].get('href')
-                    html = requests.get(album_site).text
-                    soup = BeautifulSoup(html, 'html.parser')
-                    try:
-                        year = int(soup.find('p').text.split('Year: ')[1][:4])
-                    except IndexError:
-                        year = None
-                    album_dates[albums[index].upper()] = year
-                    rap_albums.remove(rap_album)
+    start = 0
+    try:
+        for decade in decades:
+            for num in range(0, 10):
+                album_names = []
+                year = decade[:3]+str(num)
+                print(year)
+                if year == '2022':
                     break
-        dates.append(album_dates[albums[index].upper()])
-    songs = songs.assign(Date=dates)
-    songs.to_json('songs_dt2.json', orient='table', indent=4)
+                url = 'https://www.discogs.com/search/?limit=250&genre_exact' \
+                      '=Hip+Hop&decade=' + decade + '&year=' + year + \
+                      '&layout=sm&type=release'
+                while True:
+                    webD.get(url)
+                    if start == 0:
+                        webD.find_element_by_xpath(
+                            '/html/body/div[6]/div[3]/div/div['
+                            '1]/div/div[2]/div/button[2]').click()
+                        webD.find_element_by_xpath(
+                            '/html/body/div[6]/div[2]/div[3]/div[1]/button').click()
+                        start = 1
+                    for name in webD.find_elements_by_class_name(
+                            'search_result_title'):
+                        album_names.append(name.text.split(' (')[0].upper())
+                    try:
+                        url = webD.find_element_by_class_name(
+                            'pagination_next').get_attribute('href')
+                    except NoSuchElementException:
+                        break
+                for index in range(len(albums)):
+                    if albums[index].split(' (')[0].upper() in album_names:
+                        if type(dates[index]) is not float and type(dates[index]) is not float:
+                            dates[index] = int(year)
+        songs = songs.assign(Date=dates)
+        songs.to_json('songs_dt2.json', orient='table', indent=4)
+    except WebDriverException:
+        songs = songs.assign(Date=dates)
+        songs.to_json('songs_dt2.json', orient='table', indent=4)
 
 
-get_dates('https://www.rapmusicguide.com/browse/sb/ar%7CASC%7C500%7C0')
+
+get_dates()
