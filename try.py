ce3ec48573dedd0dd2e35afbc2f4094ccd327522
Dekel-Katz <84238555+Dekel-Katz@users.noreply.github.com>
2021-08-09 12:19:04 +0300

    Changed the type for the dates from strings to numbers

--- try.py
+++ try.py
@@ -1,13 +1,29 @@
 import pandas as pd
 
-df = pd.read_json('songs_dt.json', orient='table')
-dates = list(df['Date'])
-print(dates)
-for ind in range(len(dates)):
-    real_year = str(dates[ind])[:4]
-    if real_year == '1970':
-        real_year = 'NaT'
-    dates[ind] = real_year
-print(dates)
-# df = df.assign(Date=dates)
-# df.to_json('songs_dt5.json', orient='table', indent=4)
+
+def read_wordlist_file(filename):
+    """
+    Reads the words' list file and returns its insides as a list
+    :param filename: The name of the words file
+    :return: A list contains the words from the file
+    """
+    words = open(filename)
+    lines = words.readlines()
+    words.close()
+    for ind in range(len(lines)):
+        if '"Date":' in lines[ind]:
+            if 'NaT' in lines[ind]:
+                lines[ind] = lines[ind].replace('NaT', 'nan')
+    new_file_contents = "".join(lines)
+    my_file = open('songs_dt2.json', "w")
+    my_file.write(new_file_contents)
+    my_file.close()
+
+
+def read_jas(filename):
+    df = pd.read_json(filename, orient='table')
+    print(df['Date'])
+
+
+# read_wordlist_file('songs_dt2.json')
+read_jas('songs_dt5.json')
